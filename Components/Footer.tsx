0504770e6b91525b27970204037d81e6877d88df
tdepasquale <53018681+tdepasquale@users.noreply.github.com>
2020-11-01 21:50:16 -0500

    Added homepage animation.

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,22 +1,22 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
+const Container = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FooterLink = styled.a`
+  padding: 1em 2em;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 export default function Footer() {
-  const Footer = styled.footer`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const FooterLink = styled.a`
-    padding: 1em 2em;
-    &:hover {
-      text-decoration: underline;
-    }
-  `;
-
   return (
-    <Footer>
+    <Container>
       <Link href="/">
         <FooterLink>Home</FooterLink>
       </Link>
@@ -26,6 +26,6 @@ export default function Footer() {
       <Link href="/privacy-policy">
         <FooterLink>Privacy Policy</FooterLink>
       </Link>
-    </Footer>
+    </Container>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,148 @@
 import Head from 'next/head';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Link from 'next/link';
 
 const HeroContainer = styled.div`
   min-height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
   background-color: var(--color-blue);
+  overflow-x: hidden;
+
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+  }
 `;
 
-const Heading = styled.h1`
-  font-size: 6rem;
+const LeftContainer = styled.div`
+  height: 100%;
+  width: 45vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: var(--color-blue);
+
+  @media screen and (max-width: 1000px) {
+    width: 95vw;
+  }
+`;
+
+const CenterContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+
+  @media screen and (max-width: 1000px) {
+    justify-content: center;
+  }
+`;
+
+const RightContainer = styled.div`
+  min-height: 100vh;
+  width: 45vw;
+  background-color: var(--color-blue);
+  background: url('./robot.gif');
+  background-size: contain;
+  background-position: right center;
+  background-repeat: no-repeat;
+
+  @media screen and (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+
+  @media screen and (max-width: 1000px) {
+    align-items: center;
+  }
+`;
+
+const Heading = styled.h1<{ underline?: boolean }>`
+  position: relative;
+  font-size: 10rem;
+  font-weight: 900;
+  line-height: 1.2;
+
+  @media screen and (max-width: 1400px) {
+    font-size: 7rem;
+  }
+  @media screen and (max-width: 1000px) {
+    font-size: 5rem;
+  }
+`;
+
+const SlideAnim = styled.span<{ delay?: number }>`
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: -5px;
+    width: 105%;
+    height: 10rem;
+    background-color: var(--color-blue);
+    animation: slide 0.5s ease-out forwards;
+
+    ${(props) =>
+      props.delay &&
+      css`
+        animation-delay: ${props.delay}s;
+      `}
+  }
+
+  @keyframes slide {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(150%);
+    }
+  }
+`;
+
+const FadeInAnim = styled.span<{ delay?: number }>`
+  opacity: 0;
+  animation: fadeIn 0.5s ease-out forwards;
+  margin: 2rem 0 4rem 0;
+  display: block;
+
+  ${(props) =>
+    props.delay &&
+    css`
+      animation-delay: ${props.delay}s;
+    `}
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const Subheading = styled.p`
+  font-size: 2rem;
+  margin: 2rem 0;
+  text-align: left;
+
+  @media screen and (max-width: 1000px) {
+    max-width: 30rem;
+    text-align: center;
+  }
 `;
 
 const DemoButton = styled.a`
   padding: 1em 2em;
-  margin: 4rem;
+  margin: 2rem 0 4rem 0;
   background-color: var(--color-pink);
   color: white;
   font-size: 2rem;
@@ -44,10 +168,33 @@ export default function Home() {
 
       <main>
         <HeroContainer>
-          <Heading>Welcome to the Trivia Zone!</Heading>
-          <Link href="/demo">
-            <DemoButton>Try Demo</DemoButton>
-          </Link>
+          <LeftContainer>
+            <CenterContainer>
+              <Container>
+                <Heading className="z-5">
+                  <SlideAnim delay={0}>Welcome</SlideAnim>
+                </Heading>
+                <Heading className="z-4">
+                  <SlideAnim delay={0.2}>to the</SlideAnim>
+                </Heading>
+                <Heading>
+                  <SlideAnim delay={0.4}>Trivia Zone!</SlideAnim>
+                </Heading>
+                <Subheading>
+                  <FadeInAnim delay={0.6}>
+                    An alternate reality in which the only thing that matters is
+                    trivia.
+                  </FadeInAnim>
+                </Subheading>
+                <FadeInAnim delay={0.8}>
+                  <Link href="/demo">
+                    <DemoButton>Try Demo</DemoButton>
+                  </Link>
+                </FadeInAnim>
+              </Container>
+            </CenterContainer>
+          </LeftContainer>
+          <RightContainer className="z-6" />
         </HeroContainer>
       </main>
     </>
